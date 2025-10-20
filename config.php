8bc636baa4e16789f14d7ccf17d893e5a9b8f6b6
Прикладная математика и информатика <student_inf@sakhgu.net>
2025-10-20 14:14:15 +1100

    Added file config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+return ['db' => ['user'=>'root', 'dsn'=>'http://localhost/phpmyadmin/index.php?route=/database/structure&db=taskapp', 
+'pass'=>'', 'options'=>''],'storage' => storage\task.json, 
+'repository' => 'указать какой тип репозитория используется']
+?>
