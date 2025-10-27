5b66fa795879e96442154069ecec0d11cacd5a7c
Прикладная математика и информатика <student_inf@sakhgu.net>
2025-10-27 12:57:43 +1100

    Изменения

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return ['db' => ['user'=>'root', 'dsn'=>'http://localhost/phpmyadmin/index.php?route=/database/structure&db=taskapp', 
-'pass'=>'', 'options'=>''],'storage' => storage\task.json, 
+'pass'=>'', 'options'=>''],'storage' => 'storage\task.json', 
 'repository' => 'указать какой тип репозитория используется']
 ?>
