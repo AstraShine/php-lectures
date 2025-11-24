963a3edc32e3bb87531b5388cd3f5a342e3b280b
Прикладная математика и информатика <student_inf@sakhgu.net>
2025-11-24 13:55:48 +1100

    Update

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config = ['db' => ['user'=>'root', 'dsn'=>'mysql:host=localhost;dbname=taskapp;charset=utf8', 
+'pass'=>'', 'options'=>''],'storage' => 'storage\task.json', 
+'repository' => 'указать какой тип репозитория используется']
+?>
